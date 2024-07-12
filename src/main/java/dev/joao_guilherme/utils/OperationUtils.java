d30a6c5c63d56c954797c53f59e027b372d1b1bb
João Guilherme <jaaumgui@gmail.com>
2024-07-12 17:41:46 -0300

    feat(OperationUtils): create a utils class related to Operator

---
+++ src/main/java/dev/joao_guilherme/utils/OperationUtils.java
@@ -0,0 +1,19 @@
+package dev.joao_guilherme.utils;
+
+import dev.joao_guilherme.operators.BinaryOperation;
+import dev.joao_guilherme.operators.Operator;
+import dev.joao_guilherme.operators.UnaryOperation;
+
+import java.math.BigDecimal;
+import java.util.Stack;
+
+public abstract class OperationUtils {
+
+    public static void applyOperator(Operator lastOp, Stack<BigDecimal> values) {
+        switch (lastOp) {
+            case UnaryOperation uOp -> values.push(uOp.apply(values.pop()));
+            case BinaryOperation bOp -> values.push(bOp.apply(values.pop(), values.isEmpty() ? BigDecimal.ZERO : values.pop()));
+            default -> throw new IllegalStateException("Unexpected value: " + lastOp);
+        }
+    }
+}
