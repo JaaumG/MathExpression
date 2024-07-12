258e1872ceb5659a516379dbcb7ce16b9de9afdf
João Guilherme <jaaumgui@gmail.com>
2024-07-11 22:00:43 -0300

    refactor(Expression): added a new constructor and ExpressionEvaluator as field of class

--- src/main/java/dev/joao_guilherme/Expression.java
+++ src/main/java/dev/joao_guilherme/Expression.java
@@ -1,19 +1,30 @@
 package dev.joao_guilherme;
 
+
+import dev.joao_guilherme.evaluators.ArithmeticExpressionEvaluator;
+import dev.joao_guilherme.evaluators.ExpressionEvaluator;
+
 import java.math.BigDecimal;
-import java.math.MathContext;
 
 public class Expression {
 
     private final String expression;
-    private static final MathContext MATH_CONTEXT = MathContext.DECIMAL128;
-    private static final BigDecimal ZERO = BigDecimal.ZERO;
+    private final ExpressionEvaluator evaluator;
 
     public Expression(String expression) {
+        this(expression, new ArithmeticExpressionEvaluator());
+    }
+
+    public Expression(String expression, ExpressionEvaluator evaluator) {
         if (expression == null) {
             throw new IllegalArgumentException("Expression cannot be null");
         }
         this.expression = expression.replaceAll("\\s+", "");
+        this.evaluator = evaluator;
+    }
+
+    public BigDecimal evaluate() {
+        return this.evaluator.evaluate(expression);
     }
 
     public String getExpression() {
