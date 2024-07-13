d6d62e29a36f78095e929d178b8a140fd3675d49
João Guilherme <jaaumgui@gmail.com>
2024-07-12 22:52:08 -0300

    refactor(BinaryOperation/UnaryOperation): extends Operator

--- src/main/java/dev/joao_guilherme/operators/BinaryOperation.java
+++ src/main/java/dev/joao_guilherme/operators/BinaryOperation.java
@@ -3,7 +3,7 @@ package dev.joao_guilherme.operators;
 import java.math.BigDecimal;
 
 @FunctionalInterface
-public interface BinaryOperation {
+public interface BinaryOperation extends Operator {
 
     BigDecimal apply(BigDecimal a, BigDecimal b);
 }
--- src/main/java/dev/joao_guilherme/operators/UnaryOperation.java
+++ src/main/java/dev/joao_guilherme/operators/UnaryOperation.java
@@ -3,7 +3,7 @@ package dev.joao_guilherme.operators;
 import java.math.BigDecimal;
 
 @FunctionalInterface
-public interface UnaryOperation {
+public interface UnaryOperation extends Operator {
 
     BigDecimal apply(BigDecimal a);
 }
