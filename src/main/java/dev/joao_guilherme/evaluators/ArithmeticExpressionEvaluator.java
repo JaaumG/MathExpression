de78ce29e15bcff11b419c2006655e3e9b70c203
João Guilherme <jaaumgui@gmail.com>
2024-07-12 22:57:20 -0300

    refactor(ArithmeticExpressionEvaluator): big refactor, i was changing as need to make it keep work correctly

--- src/main/java/dev/joao_guilherme/evaluators/ArithmeticExpressionEvaluator.java
+++ src/main/java/dev/joao_guilherme/evaluators/ArithmeticExpressionEvaluator.java
@@ -1,63 +1,104 @@
 package dev.joao_guilherme.evaluators;
 
-import dev.joao_guilherme.factory.OperatorFactory;
+import dev.joao_guilherme.functions.LogarithmFunction;
+import dev.joao_guilherme.functions.NaturalLogarithmFunction;
+import dev.joao_guilherme.functions.NthRootFunction;
+import dev.joao_guilherme.functions.SquareRootFunction;
 import dev.joao_guilherme.operators.*;
+import dev.joao_guilherme.utils.BigDecimalUtils;
+import dev.joao_guilherme.utils.FunctionUtils;
 
 import java.math.BigDecimal;
 import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static dev.joao_guilherme.utils.ExpressionUtils.*;
+import static dev.joao_guilherme.utils.OperationUtils.applyOperator;
 
-public class ArithmeticExpressionEvaluator extends ExpressionEvaluator {
+public class ArithmeticExpressionEvaluator implements ExpressionEvaluator {
 
-    private static Stack<BigDecimal> VALUES = new Stack<>();
-    private static Stack<Operator> OPS = new Stack<>();
-    private static final OperatorFactory operatorFactory = OperatorFactory.getInstance();
+    private static final Pattern NUMBER_PATTERN = Pattern.compile("-?\\d+(\\.\\d+)?");
+    private static final Pattern FUNCTION_PATTERN = Pattern.compile("[a-zA-Z]+");
+
+
+    public ArithmeticExpressionEvaluator() {
+        this.functions.put("sqrt", new SquareRootFunction());
+        this.functions.put("nrt", new NthRootFunction());
+        this.functions.put("ln" ,new NaturalLogarithmFunction());
+        this.functions.put("log", new LogarithmFunction());
+        this.operator.put("+", new AdditionOperator());
+        this.operator.put("-", new SubtractOperator());
+        this.operator.put("/", new DivideOperator());
+        this.operator.put("*", new MultiplyOperator());
+        this.operator.put("^", new ExponentialOperator());
+        this.operator.put("!", new FactorialOperator());
+        this.variables.put("e", BigDecimalUtils.E);
+        this.variables.put("pi", BigDecimalUtils.PI);
+    }
 
     @Override
-    public BigDecimal evaluate(String expression) {
-        if (expression == null) {
-            throw new IllegalArgumentException("Expression cannot be null");
-        }
-        VALUES = new Stack<>();
-        OPS = new Stack<>();
+    public BigDecimal evaluate(final String expression) {
+        if (expression == null) throw new IllegalArgumentException("Expression cannot be null");
 
-        for (int i = 0; i < expression.length(); i++) {
+        Stack<BigDecimal> values = new Stack<>();
+        Stack<Operator> ops = new Stack<>();
+        int i = 0;
+        while (i < expression.length()) {
             char c = expression.charAt(i);
 
             if (isOpeningBracket(c)) {
                 int j = getIndexClosingBracket(expression, i);
-                BigDecimal result = evaluate(expression.substring(i + 1, j));
-                VALUES.push(result);
-                i = j;
-            } else if (isDigit(c) || (c == '-' && isMinusSignNegation(expression, i))) {
-                int start = i++;
-                while (i < expression.length() && (Character.isDigit(expression.charAt(i)) || expression.charAt(i) == '.')) {
-                    i++;
+                values.push(evaluate(expression.substring(i + 1, j)));
+                i = j + 1;
+            } else if (isDigit(c) || (c == '-' && isMinusSignNegation(expression, i, this))) {
+                checkForImplicitMultiplication(ops, i, expression);
+                Matcher matcher = NUMBER_PATTERN.matcher(expression.substring(i));
+                if (matcher.find()) {
+                    String number = matcher.group();
+                    values.push(new BigDecimal(number));
+                    i += number.length();
                 }
-                VALUES.push(new BigDecimal(expression.substring(start, i--)));;
-            } else if (OperatorFactory.getInstance().isOperator(c)) {
-                Operator op = operatorFactory.createOperator(c);
-                while (!OPS.empty() && OPS.peek().hasHigherPrecedence(op)) {
-                    applyOperator();
+            } else if (isOperator(String.valueOf(c))) {
+                Operator op = operator.get(String.valueOf(c));
+                while (!ops.empty() && ops.peek().hasHigherPrecedence(op)) applyOperator(ops.pop(), values);
+                ops.push(op);
+                i++;
+            } else if (isCharacter(c)) {
+                Matcher matcher = FUNCTION_PATTERN.matcher(expression.substring(i));
+                if (matcher.find()) {
+                    String string = matcher.group();
+                    if (isFunction(string)) {
+                        checkForImplicitMultiplication(ops, i, expression);
+                        i += string.length();
+                        if (i < expression.length() && isOpeningBracket(expression.charAt(i))) {
+                            BigDecimal result = FunctionUtils.evaluateFunction(this, expression, i - string.length(), i);
+                            values.push(result);
+                            i = getIndexClosingBracket(expression, i) + 1; // Move to the closing bracket index
+                        } else {
+                            throw new IllegalArgumentException("Invalid function: " + string);
+                        }
+                    } else if (isVariable(string)) {
+                        checkForImplicitMultiplication(ops, i, expression);
+                        i += string.length();
+                        values.push(getVariable(string));
+                    } else {
+                        throw new IllegalArgumentException("Invalid function: " + string);
+                    }
                 }
-                OPS.push(op);
+            } else {
+                i++;
             }
         }
 
-        while (!OPS.empty()) {
-            applyOperator();
-        }
+        while (!ops.empty()) applyOperator(ops.pop(), values);
 
-        return VALUES.pop();
+        return values.pop().stripTrailingZeros();
     }
 
-    private void applyOperator() {
-        Operator lastOp = OPS.pop();
-        switch (lastOp) {
-            case UnaryOperation uOp -> VALUES.push(uOp.apply(VALUES.pop()));
-            case BinaryOperation bOp -> VALUES.push(bOp.apply(VALUES.pop(), VALUES.isEmpty() ? BigDecimal.ZERO : VALUES.pop()));
-            default -> throw new IllegalStateException("Unexpected value: " + lastOp);
+    private static void checkForImplicitMultiplication(Stack<Operator> ops, int i, String expression) {
+        if ((i > 0 && (isDigit(expression.charAt(i - 1)) || isClosingBracket(expression.charAt(i - 1)))) || (expression.length() > i + 1 && isOpeningBracket(expression.charAt(i + 1)))) {
+            ops.push(new MultiplyOperator());
         }
     }
 }
