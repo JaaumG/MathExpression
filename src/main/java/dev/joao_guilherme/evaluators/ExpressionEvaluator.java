a7aef3e36feeb3970308849517559dcacaca65a8
João Guilherme <jaaumgui@gmail.com>
2024-07-12 22:54:05 -0300

    refactor(ExpressionEvaluator): abstract class turn into interface with methods to easily pass operators variables and functions to it implementations

--- src/main/java/dev/joao_guilherme/evaluators/ExpressionEvaluator.java
+++ src/main/java/dev/joao_guilherme/evaluators/ExpressionEvaluator.java
@@ -1,8 +1,79 @@
 package dev.joao_guilherme.evaluators;
 
+import dev.joao_guilherme.functions.Function;
+import dev.joao_guilherme.operators.BinaryOperation;
+import dev.joao_guilherme.operators.Operator;
+import dev.joao_guilherme.operators.UnaryOperation;
+
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
-public abstract class ExpressionEvaluator {
+public interface ExpressionEvaluator {
 
-    public abstract BigDecimal evaluate(String expression);
+    Map<String, Operator> operator = new HashMap<>();
+    Map<String, Function> functions = new HashMap<>();
+    Map<String, BigDecimal> variables = new HashMap<>();
+
+    BigDecimal evaluate(String expression);
+
+    default void addFunction(String function, Function functionImpl) {
+        this.functions.put(function, functionImpl);
+    }
+
+    default boolean isOperator(String operator) {
+        return this.operator.containsKey(operator);
+    }
+
+    default Operator getOperator(String operator) {
+        return this.operator.get(operator);
+    }
+
+    default boolean isFunction(String function) {
+        return this.functions.containsKey(function);
+    }
+
+    default Function getFunction(String function) {
+        return functions.get(function);
+    }
+
+    default boolean isVariable(String variable) {
+        return variables.containsKey(variable);
+    }
+
+    default BigDecimal getVariable(String variable) {
+        return variables.get(variable);
+    }
+
+    default void addVariable(String variable, BigDecimal value) {
+        variables.put(variable, value);
+    }
+
+    default void addOperator(String operator, int precedence, UnaryOperation operation) {
+        this.operator.put(operator, new UnaryOperation() {
+            @Override
+            public BigDecimal apply(BigDecimal a) {
+                return operation.apply(a);
+            }
+
+            @Override
+            public int getPrecedence() {
+                return precedence;
+            }
+        });
+    }
+
+    default void addOperator(String operator, int precedence, BinaryOperation operation) {
+        this.operator.put(operator, new BinaryOperation() {
+            @Override
+            public BigDecimal apply(BigDecimal b, BigDecimal a) {
+                return operation.apply(b, a);
+            }
+
+            @Override
+            public int getPrecedence() {
+                return precedence;
+            }
+        });
+    }
 }
