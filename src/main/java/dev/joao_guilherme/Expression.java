449418b742c73ffd42e8c640820e3bd6541dae14
João Guilherme <jaaumgui@gmail.com>
2024-07-12 22:56:23 -0300

    feat(Expression): Added some methods to easily add new variables operators or functions

--- src/main/java/dev/joao_guilherme/Expression.java
+++ src/main/java/dev/joao_guilherme/Expression.java
@@ -3,6 +3,9 @@ package dev.joao_guilherme;
 
 import dev.joao_guilherme.evaluators.ArithmeticExpressionEvaluator;
 import dev.joao_guilherme.evaluators.ExpressionEvaluator;
+import dev.joao_guilherme.functions.Function;
+import dev.joao_guilherme.operators.BinaryOperation;
+import dev.joao_guilherme.operators.UnaryOperation;
 
 import java.math.BigDecimal;
 
@@ -16,9 +19,7 @@ public class Expression {
     }
 
     public Expression(String expression, ExpressionEvaluator evaluator) {
-        if (expression == null) {
-            throw new IllegalArgumentException("Expression cannot be null");
-        }
+        if (expression == null) throw new IllegalArgumentException("Expression cannot be null");
         this.expression = expression.replaceAll("\\s+", "");
         this.evaluator = evaluator;
     }
@@ -29,5 +30,39 @@ public class Expression {
 
     public String getExpression() {
         return expression;
+    }
+
+    public Expression withFunction(String function, Function functionImpl) {
+        evaluator.addFunction(function, functionImpl);
+        return this;
+    }
+
+    public Expression withVariable(String variable, BigDecimal value) {
+        evaluator.addVariable(variable, value);
+        return this;
+    }
+
+    public Expression withVariable(String variable, String value) {
+        return new Expression(expression.replaceAll(variable, value), evaluator);
+    }
+
+    public Expression withOperator(String operator, int precedence, BinaryOperation operation) {
+        evaluator.addOperator(operator, precedence, operation);
+        return this;
+    }
+
+    public Expression withOperator(String operator, int precedence, UnaryOperation operation) {
+        evaluator.addOperator(operator, precedence, operation);
+        return this;
+    }
+
+    public Expression withOperator(String operator, BinaryOperation operation) {
+        evaluator.addOperator(operator, 1, operation);
+        return this;
+    }
+
+    public Expression withOperator(String operator, UnaryOperation operation) {
+        evaluator.addOperator(operator, 1, operation);
+        return this;
     }
 }
