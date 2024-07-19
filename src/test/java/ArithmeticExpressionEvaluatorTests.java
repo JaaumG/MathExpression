1eb69c30407446a53977166002a22b828427125b
João Guilherme <jaaumgui@gmail.com>
2024-07-19 15:27:28 -0300

    test(ArithmeticExpressionEvaluatorTests): Renamed test class

--- src/test/java/ArithmeticExpressionEvaluatorTests.java
+++ src/test/java/ArithmeticExpressionEvaluatorTests.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ExpressionTests {
+public class ArithmeticExpressionEvaluatorTests {
 
     @Test
     @DisplayName("Basic addition")
