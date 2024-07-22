dbf578d13da861ce3c311c7cefe15157e4bc3639
João Guilherme <jaaumgui@gmail.com>
2024-07-22 14:16:04 -0300

    refactor(FunctionUtils): Simplified method to evaluate Function, now using a new method that split a string in commas but only if the comma is not inside another bracket, because this might be another function

--- src/main/java/dev/joao_guilherme/utils/FunctionUtils.java
+++ src/main/java/dev/joao_guilherme/utils/FunctionUtils.java
@@ -4,7 +4,9 @@ import dev.joao_guilherme.evaluators.ExpressionEvaluator;
 import dev.joao_guilherme.functions.Function;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static dev.joao_guilherme.utils.ExpressionUtils.getIndexClosingBracket;
 
@@ -14,22 +16,44 @@ public abstract class FunctionUtils {
         int j = getIndexClosingBracket(expression, i);
         String innerExpression = expression.substring(i + 1, j);
         Function func = evaluator.getFunction(expression.substring(start, i));
-        BigDecimal result;
 
         if (innerExpression.contains(",")) {
-            String[] parts = innerExpression.split(",");
-            BigDecimal[] args = Arrays.stream(parts).map(a -> {
-                if (evaluator.isVariable(a)) {
-                    return evaluator.getVariable(a);
-                } else {
-                    return new BigDecimal(a);
-                }
-            }).toArray(BigDecimal[]::new);
-            result = applyFunction(func, args);
+            String[] parts = splitPreservingGroups(innerExpression);
+            BigDecimal[] args = Arrays.stream(parts).map(evaluator::evaluate).toArray(BigDecimal[]::new);
+            return applyFunction(func, args);
         } else {
-            result = applyFunction(func, evaluator.evaluate(innerExpression));
+            return applyFunction(func, evaluator.evaluate(innerExpression));
         }
-        return result;
+    }
+
+    public static String[] splitPreservingGroups(String str) {
+        List<String> result = new ArrayList<>();
+        int openParentheses = 0;
+        StringBuilder currentSegment = new StringBuilder();
+
+        for (char ch : str.toCharArray()) {
+            switch (ch) {
+                case ',' -> {
+                    if (openParentheses == 0) {
+                        result.add(currentSegment.toString().trim());
+                        currentSegment.setLength(0);
+                    } else {
+                        currentSegment.append(ch);
+                    }
+                }
+                case '(', '[', '{' -> {
+                    openParentheses++;
+                    currentSegment.append(ch);
+                }
+                case ')', ']', '}' -> {
+                    openParentheses--;
+                    currentSegment.append(ch);
+                }
+                default -> currentSegment.append(ch);
+            }
+        }
+        result.add(currentSegment.toString().trim());
+        return result.toArray(new String[0]);
     }
 
     public static BigDecimal applyFunction(Function function, BigDecimal... args) {
