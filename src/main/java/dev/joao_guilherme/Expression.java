ab15c564fa99fb05e1748013b57d9cbc610955f7
João Guilherme <jaaumgui@gmail.com>
2024-07-25 11:05:59 -0300

    feat(Expression): added new method to derivate an expression based on value for X

--- src/main/java/dev/joao_guilherme/Expression.java
+++ src/main/java/dev/joao_guilherme/Expression.java
@@ -2,11 +2,13 @@ package dev.joao_guilherme;
 
 
 import dev.joao_guilherme.evaluators.ArithmeticExpressionEvaluator;
+import dev.joao_guilherme.evaluators.DerivativeEvaluator;
 import dev.joao_guilherme.evaluators.EquationEvaluator;
 import dev.joao_guilherme.evaluators.ExpressionEvaluator;
 import dev.joao_guilherme.functions.Function;
 import dev.joao_guilherme.operators.BinaryOperation;
 import dev.joao_guilherme.operators.UnaryOperation;
+import dev.joao_guilherme.utils.BigDecimalUtils;
 
 import java.math.BigDecimal;
 
@@ -36,6 +38,31 @@ public class Expression {
     public BigDecimal solveFor(String variable) {
         return EquationEvaluator.solve(expression, variable, evaluator);
     }
+
+    public BigDecimal derivativeForX(String x) {
+        return derivate("x", x);
+    }
+
+    public BigDecimal derivativeForX(long x) {
+        return derivate("x", x);
+    }
+
+    public BigDecimal derivativeForX(double x) {
+        return derivate("x", x);
+    }
+
+    public BigDecimal derivate(String variable, String x) {
+        return DerivativeEvaluator.derivate(evaluator, expression, variable, BigDecimalUtils.valueOf(x));
+    }
+
+    public BigDecimal derivate(String variable, long x) {
+        return DerivativeEvaluator.derivate(evaluator, expression, variable, BigDecimalUtils.valueOf(x));
+    }
+
+    public BigDecimal derivate(String variable, double x) {
+        return DerivativeEvaluator.derivate(evaluator, expression, variable, BigDecimalUtils.valueOf(x));
+    }
+
 
     public String getExpression() {
         return expression;
