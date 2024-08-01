3a350789b1925fa266295933a373169cfacd16f4
João Guilherme <jaaumgui@gmail.com>
2024-08-01 12:05:49 -0300

    style(PercentageBasedAdditionEvaluator): fix spacing

--- src/main/java/dev/joao_guilherme/evaluators/PercentageBasedAdditionEvaluator.java
+++ src/main/java/dev/joao_guilherme/evaluators/PercentageBasedAdditionEvaluator.java
@@ -11,7 +11,7 @@ public class PercentageBasedAdditionEvaluator extends ArithmeticExpressionEvalua
     @Override
     protected void percentageHandler(PercentageOperator pop, Stack<Operator> ops, Stack<BigDecimal> values, String expression, int i) {
         if (values.isEmpty()) throw new IllegalArgumentException("Invalid expression: " + expression);
-        if (i + 1 < expression.length() && (expression.charAt(i + 1) == '*' || expression.charAt(i + 1) == '/' || expression.charAt(i + 1) == '(')){
+        if (i + 1 < expression.length() && (expression.charAt(i + 1) == '*' || expression.charAt(i + 1) == '/' || expression.charAt(i + 1) == '(')) {
             values.push(pop.apply(values.pop()));
         } else {
             pop.applyImplicitPercentageOperator(values, ops);
