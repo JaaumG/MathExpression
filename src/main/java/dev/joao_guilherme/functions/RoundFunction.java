36dfeaf445e22ac000fc8e8c977dd0b97633ebb8
João Guilherme <mail@joaoguilherme.dev>
2025-03-21 13:42:32 -0300

    feat(RoundFunction): Added round function

---
+++ src/main/java/dev/joao_guilherme/functions/RoundFunction.java
@@ -0,0 +1,12 @@
+package dev.joao_guilherme.functions;
+
+import dev.joao_guilherme.utils.BigDecimalUtils;
+
+import java.math.BigDecimal;
+
+public class RoundFunction implements Function {
+    @Override
+    public BigDecimal apply(BigDecimal... args) {
+        return BigDecimalUtils.round(args[0]);
+    }
+}
