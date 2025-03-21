b6ccd0f78146be9a04ca97d4681e26778cd8c982
João Guilherme <mail@joaoguilherme.dev>
2025-03-21 19:25:28 -0300

    refactor(Function) defined as sealed, added new methods and throwing exceptions on apply to be overried

--- src/main/java/dev/joao_guilherme/functions/Function.java
+++ src/main/java/dev/joao_guilherme/functions/Function.java
@@ -2,8 +2,35 @@ package dev.joao_guilherme.functions;
 
 import java.math.BigDecimal;
 
-@FunctionalInterface
-public interface Function {
+public sealed interface Function permits UnaryFunction, BinaryFunction, VarArgsFunction {
 
-    BigDecimal apply(BigDecimal... args);
+    default int minArgs() {
+        return 0;
+    }
+
+    default int maxArgs() {
+        return Integer.MAX_VALUE;
+    }
+
+    default BigDecimal apply(BigDecimal a) {
+        throw new IllegalArgumentException("Invalid number of arguments: 1");
+    }
+
+    default BigDecimal apply(BigDecimal a, BigDecimal b) {
+        throw new IllegalArgumentException("Invalid number of arguments: 2");
+    }
+
+    default BigDecimal apply(BigDecimal... args) {
+        if (args.length == 1) {
+            return apply(args[0]);
+        } else if (args.length == 2) {
+            return apply(args[0], args[1]);
+        } else {
+            throw new IllegalArgumentException("Invalid number of arguments: " + args.length);
+        }
+    }
+
+    default boolean isValidArgsCount(int argsCount) {
+        return argsCount >= minArgs() && argsCount <= maxArgs();
+    }
 }
